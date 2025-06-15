37250d26f02d0aa83db7fe8fcbbbbf910bb2382b
asenetcky <me@asenetcky.dev>
2025-06-15 11:17:03 +0000

    workign through tutorial

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, col, create_engine, select
 
 
 class Hero(SQLModel, table=True):
@@ -22,26 +22,38 @@ def create_heroes():
     hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
     hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
     hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+    hero_4 = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
+    hero_5 = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
+    hero_6 = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
+    hero_7 = Hero(name="Captain North America", secret_name="Esteban Rogelios", age=93)
 
     with Session(engine) as session:
         session.add(hero_1)
         session.add(hero_2)
         session.add(hero_3)
+        session.add(hero_4)
+        session.add(hero_5)
+        session.add(hero_6)
+        session.add(hero_7)
 
         session.commit()
 
 
 def select_heroes():
     with Session(engine) as session:
-        statement = select(Hero)
-        results = session.exec(statement)
-        for hero in results:
-            print(hero)
+        heroes = session.exec(
+            # there combine with AND - can also be inside single where statement
+            # select(Hero).where(Hero.age >= 35).where(Hero.age < 40)
+            # select(Hero).where(or_(Hero.age <= 35, Hero.age > 90))
+            select(Hero).where(col(Hero.age) >= 35)
+        ).all()
+        print(heroes)
 
 
 def main():
     create_db_and_tables()
     create_heroes()
+    select_heroes()
 
 
 if __name__ == "__main__":
